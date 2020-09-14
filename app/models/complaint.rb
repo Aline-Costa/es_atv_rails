7c12811d3cded05d3502de40a300ce7669dd80ae
Aline Costa <linecosta04@gmail.com>
2020-09-13 22:04:55 -0300

    creating validations for complaints form

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -1,2 +1,6 @@
 class Complaint < ApplicationRecord
+
+	validates :title, :description, :category, :company, presence:true
+	validates_length_of :title, maximum: 50
+	validates_length_of :category, maximum: 40
 end
